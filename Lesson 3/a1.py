f2f159fdc9ea8556863e5405d8565e6498d0355f
Lalila-Diya <diyaj.696969@gmail.com>
2025-12-09 21:04:27 +0530

    Add files via upload

---
+++ Lesson 3/a1.py
@@ -0,0 +1,11 @@
+a=25
+print("a=",a, type(a))
+
+b=38.4
+print("b=",b, type(b))
+
+c=True
+print("c=",c, type(c))
+
+d="Hello"
+print("d=",d, type(d))
---
+++ Lesson 3/a2.py
@@ -0,0 +1,9 @@
+a=input("Enter you age: ")
+print("Your age is: ", a)
+print("a=",a, type(a))
+
+b=int(a)
+print("Datatype of a after conversion: ", type(b))
+
+height=float(input("Enter your height: "))
+print("Your height is: ", height, type(height))
